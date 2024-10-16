d039b81223ab8f2b0ddc91f80ff5efdb4fcddfe7
Nashir <johndoe@example.com>
2024-10-16 21:29:41 +0100

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://9996b9238c84356fa91b0600dca0a37a@o4508106451582976.ingest.de.sentry.io/4508106477666384",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration(),
+  ],
+  
+  tracesSampleRate: 1.0, 
+  
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+
+  replaysSessionSampleRate: 0.1, 
+  replaysOnErrorSampleRate: 1.0, 
+});
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
